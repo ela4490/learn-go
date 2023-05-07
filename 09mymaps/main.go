ae5b8f04109c4bf14431bfe6cd8453ecf50a7ff9
Ela Singh <30351771+ela4490@users.noreply.github.com>
2023-05-07 16:53:18 +0200

    Maps in golang

---
+++ 09mymaps/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Maps in golang")
+	languages := make(map[string]string)
+
+	languages["JS"] = "Javascript"
+	languages["RB"] = "Ruby"
+	languages["PY"] = "Python"
+	fmt.Println("List of all languages :", languages)
+	fmt.Println("JS shorts for :", languages["JS"])
+
+	delete(languages, "RB")
+	fmt.Println("List of all launguages :", languages)
+
+	// Loops are interesting in golang
+	for key, value := range languages {
+		fmt.Printf("For key %v, value is %v\n", key, value)
+	}
+
+	// With comma ok syntax
+	for _, value := range languages {
+		fmt.Printf("For key v, value is %v\n", value)
+	}
+}
